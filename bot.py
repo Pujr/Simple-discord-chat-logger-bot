f6c296e6d521668401fb06db2b7e80940fe50a81
Pujr <42500039+Pujr@users.noreply.github.com>
2018-11-14 01:18:39 -0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+#Insert your own discord bot token.
+
+TOKEN = "NTEyMTYzMjUyNzcyNTM2MzQy.Ds1lAw.48sz8evPWz-uDWsPrqcJmursl7M"
+
+client = commands.Bot(command_prefix = "&")
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print ("Bot is ready to go!")
+
+
+#----------------------------------------------------------    
+
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    print('{}: {}'.format(author, content))
+
+@client.event
+async def on_message_delete(message):
+    author = message.author
+    content = message.content
+    channel = message.channel
+    await client.send_message(channel, '{}: {}'.format(author, content))
+
+client.run(TOKEN)
